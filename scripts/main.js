e43428e9a889853790b18b9976adacd7007b186a
JohnBrunson <JohnFBrunson@gmail.com>
2024-10-26 20:04:39 -0700

    Added new functions.

--- scripts/main.js
+++ scripts/main.js
@@ -1,63 +1,84 @@
 
-//Global Variables TODO: Can we make these local?
-let firstName;
-let lastName;
-let salary;
-//function is working. May need to log it somewhere...
+//Refined. Now creates an object and inserts arrays into it. Also has a proper While loop. 
 function collectEmployees() {
-// retrieve user input and generates an object with First name, last name and salary. Once entered, the function should return input as an array.
-
-
-firstName = prompt("What is the employee's first Name?");
-lastName = prompt("What is the employee's last name?");
-salary = prompt("What is the employee's salary?");
-
-console.log (`INFO: Input is ${firstName} ${lastName} ${salary}`);
-};
-
-//Sanitization of input
-//TODO: Convert to loop? This is kind of ugly.
-function sanitization() {
-    if (isNaN(salary)){
-        // TODO: Refactor this. It works, but it's not pretty. I can probably compact this down a bit.
-        let continuePrompt = prompt('The Salary you have entered is not a number. Do you want to continue? Press "Y" to continue and the salary will be entered as $0.00. Press "N" to re-enter.');
-        continuePrompt.toLowerCase(); 
-        if (continuePrompt === "y"){
-            salary = 0.00
-            console.log(`INFO: Salary is ${salary}`)
-            continuePrompt = 'n';
+    let firstName;
+    let lastName;
+    let salary;
+    let continuePrompt = true;
+    let employeesArray = [];
+    
+    // The comparison must be explicit or this will not work correctly. Could probably just convert the input into lower case, but... that can happen during refactor. Right now, we want it working.
+    
+    while (continuePrompt === true) {
+      firstName = prompt("Please enter employee's first name: ");
+      lastName = prompt ("Please enter employee's last name: ");
+      salary = prompt ("Please enter the employee's salary: ");
+    
+      //Check to see if the salary is not a number. If it's not a number, default to 0.
+        if (isNaN(salary)) {
+          salary = 0.00;
         }
-        else {
-            continuePrompt = null;
-            salary = prompt("What is the employee's salary?");
-            console.log(`INFO: Salary is ${salary}`)
-            sanitization();
+        // Create employee object with the data from the user.
+        let employee = {
+            firstName: firstName,
+            lastName: lastName,
+            salary: salary
         }
+        // push the array into the object.
+        employeesArray.push(employee);
+        console.log(employeesArray);
+        // Cleanup (Is there a way to simply remove these from memory entirely? Is it worth the effort?)
+        firstName = null;
+        lastName = null;
+        salary = null;
+    
+        //Feels like there should be different buttons here. Is there a way to change the buttons for the "real" version?
+        continuePrompt = confirm("Do you wish to enter data for another employee? OK to Enter a new employee, Cancel to Stop Entering New employees.");
+        console.log(`INFO: Continue Prompt is ${continuePrompt}`);
+        console.log(employeesArray);
+      }
+      return employeesArray;
+}
+function displayAverageSalary() {
+ // TODO: Calculate and display the average salary
+let accumulator = 0;
+let numberOfEmployees = employeesArray.length;
+  // Pull the 2nd element from object 'employee array' and add to the accumulator
+    for (i = 0; i < employeesArray.length; i++){
+        console.log(accumulator);
+        accumulator += Number(employeesArray[i].salary);
     };
+console.log(`INFO: Accumulator is: ${accumulator}`);
+console.log(`INFO: numberOfEmployees is ${numberOfEmployees}`);
+
+    // Average calculator
+ let salaryAverage = accumulator / numberOfEmployees;
+ let averageSalaryWithTwoDecimals = salaryAverage.toFixed(2);
+ console.log(`INFO: salaryAverage is ${salaryAverage}`)
+ console.log(`The average employee salary between our ${numberOfEmployees} employee(s) is \$${averageSalaryWithTwoDecimals}`)
+ return salaryAverage;
 }
 
+function getRandomEmployee() {
+ // TODO: Select and display a random employee
 
-// function confirm() {
-//     let anotherEmployee = false
-//     prompt('Do you wish to enter another employee?')
-//      while (anotherEmployee === true){
-//          collectEmployees();
-//          sanitization();
+  // Generate a random number
+// NOTE: Should probably look at the return value of length to verify if it's a string or a number. It may not be 100% necessary to cast this as a number.
+const maxNumber = Number(employeesArray.length);
+console.log(maxNumber);
 
-//      }
-// }
+// Idea from MDN. Feels like there should be a more "randomish" way to do this.
+let randomNumber = Math.floor(Math.random() * maxNumber);
+console.log(`Random Number = ${randomNumber}`);
 
+// Use that Random number to pick the employee and log it.
+employeeFirstName = employeesArray[randomNumber].firstName
+console.log(`INFO: Lucky Employee First Name ${employeeFirstName}`)
+employeeLastName = employeesArray[randomNumber].lastName
+console.log (`INFO: Lucky Employee Last Name ${employeeLastName}`)
 
-    
-
-
-// displayAverageSalary() {
-//     // takes salary and averages it.
-// }
-
-// getRandomEmployee() {
-//     // Responsible for lottery drawing.
-// }
+console.log(`Congratulations to ${employeeFirstName} ${employeeLastName}, our random drawing winner!`);
+}
 
 // displayEmployees() {
 //     // take employees, sort by last name, place on table in page
